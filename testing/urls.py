3fade2e492f13a0449c9f44d6762ea7d55f786e5
Arushi Butan <arushibutan11@gmail.com>
2017-02-17 02:57:59 +0530

    Update urls.py

--- testing/urls.py
+++ testing/urls.py
@@ -4,6 +4,10 @@ urlpatterns = [
    
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^checkmatch$', views.checkmatch, name='checkmatch'),
-    url(r'^getuser$', views.getuser, name='getuser')
+    url(r'^register$', views.register, name='register'),
+    url(r'^checkproof$', views.checkproof, name='checkproof'),
+    url(r'^picupload$', views.picupload, name='picupload'),
+    url(r'^forgetpass$', views.forgetpass, name='forgetpass')
+
 
 ]
