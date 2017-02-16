3444f77126cb93e2c529712a1cecfa57a097d40b
Arushi Butan <arushibutan11@gmail.com>
2017-02-17 02:29:44 +0530

    Added forget password functionality

--- testing/views.py
+++ testing/views.py
@@ -1,5 +1,4 @@
 
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -19,18 +18,20 @@ def checkproof(request):
         checkuser = User.objects.get(id=Id)
         print checkuser.carno
         print checkuser.identity
-        if checkuser.carno is not None and checkuser.identity != "":
-            return Response("1")
+        if checkuser.carno is not None and checkuser.identity!="":
+		    return Response("1")
         else:
-            return Response("0")
+		    return Response("0")
+
+		
 
 
 @api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
 def checkmatch(request):
     if request.method == 'POST':
-        id = request.POST.get('email')
-        pwd = request.POST.get('pass')
+        id= request.POST.get('email')
+        pwd= request.POST.get('pass')
         try:
             checkuser = User.objects.get(email=id)
         except ObjectDoesNotExist:
@@ -38,8 +39,8 @@ def checkmatch(request):
         print checkuser
         if checkuser is None:
             return Response("nouser")
-
-        elif checkuser is not None:
+			
+        elif checkuser is not None:  		
             if pwd == checkuser.password:
                 response_data = {}
                 response_data['id'] = checkuser.id
@@ -47,7 +48,7 @@ def checkmatch(request):
                 return Response(response_data)
             else:
                 return Response("nomatch")
-
+			
 
 @api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
@@ -61,24 +62,68 @@ def register(request):
         Pw = request.POST.get('pass')
         user_obj = User()
 
-        user_obj.name = Name
-        user_obj.email = Email
-        user_obj.phone = Phone
-        user_obj.home_add = Homeadd
-        user_obj.carno = Carno
-        user_obj.password = Pw
+        user_obj.name=Name
+        user_obj.email=Email 
+        user_obj.phone= Phone
+        user_obj.home_add=Homeadd
+        user_obj.carno= Carno
+        user_obj.password=Pw
         user_obj.save(force_insert=True)
-        return Response("success")
+        return Response("success") 
+        
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def picupload(request):
+    if request.method == 'POST':
+        type = request.POST.get('type')
+        print type
+        image = request.POST.get('image')
+        id= request.POST.get('id')
+        try:
+            checkuser = User.objects.get(id=id)
+        except ObjectDoesNotExist:
+            checkuser = None
+        print checkuser
+        if checkuser is None:
+            return Response("nouser")
+			
+        elif checkuser is not None:  	
+            if type == "iden": 
+                checkuser.identity = image
+                checkuser.save()
+                return Response("doneiden")
+                print checkuser.identity
+            elif type == "driv":
+                checkuser.pic = image
+                checkuser.save()
+                print checkuser.pic
+                return Response("picuploaded")
 
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def forgetpass(request):
+    if request.method == 'POST':
+        id= request.POST.get('email')
+        pwd= request.POST.get('pass')
+        try:
+            checkuser = User.objects.get(email=id)
+        except ObjectDoesNotExist:
+            checkuser = None
+        print checkuser
+        if checkuser is None:
+            return Response("nouser")
+			
+        elif checkuser is not None:  		
+            checkuser.password = pwd
+            checkuser.save()
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login("arushibutan11@gmail.com", "#########")
+            msg = "Your new pssword is "+pwd +"."
+            server.sendmail("arushibutan11@gmail.com", id, msg)
+            server.quit()
+            return Response("done")
 
-
-
-
-
-
-
-
+            
 
 	
-
-		
