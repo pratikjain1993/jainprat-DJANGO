aefd6feeec203f26d0f76060c7ee2142d767c7ba
Arushi Butan <arushibutan11@gmail.com>
2017-02-17 02:31:05 +0530

    Update models.py

--- testing/models.py
+++ testing/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email= models.EmailField()
-    pic = models.FileField()
+    pic = models.FileField(null = True, max_length = 10485760)
     phone = models.IntegerField()
     home_add= models.TextField()
     carno= models.CharField(max_length=100)
-    identity = models.FileField()
+    identity = models.FileField(null = True, max_length = 10485760)
     password = models.CharField(max_length=100)
     rating = models.DecimalField(max_digits=4, decimal_places=2)
 
