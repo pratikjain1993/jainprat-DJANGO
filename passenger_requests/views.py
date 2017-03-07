619cbfaa16d5aad254c15da85560efaf674dba49
aayushi chadha <aayushi chadha>
2017-03-08 00:41:52 +0530

    Integrated Urls

--- passenger_requests/views.py
+++ passenger_requests/views.py
@@ -13,74 +13,13 @@ from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
 
 
-@api_view(['GET', 'POST'])
-@permission_classes((permissions.AllowAny,))
-def checkproof(request):
-    if request.method == 'POST':
-        Id = request.POST.get('id')
-        checkuser = User.objects.get(id=Id)
-        print checkuser.carno
-        print checkuser.identity
-        if checkuser.carno is not None and checkuser.identity != "":
-            return Response("1")
-        else:
-            return Response("0")
-
-
-@api_view(['GET', 'POST'])
-@permission_classes((permissions.AllowAny,))
-def checkmatch(request):
-    if request.method == 'POST':
-        id = request.POST.get('email')
-        pwd = request.POST.get('pass')
-        try:
-            checkuser = User.objects.get(email=id)
-        except ObjectDoesNotExist:
-            checkuser = None
-        print checkuser
-        if checkuser is None:
-            return Response("nouser")
-
-        elif checkuser is not None:
-            if pwd == checkuser.password:
-                response_data = {}
-                response_data['id'] = checkuser.id
-                response_data['name'] = checkuser.name
-                return Response(response_data)
-            else:
-                return Response("nomatch")
-
-
-@api_view(['GET', 'POST'])
-@permission_classes((permissions.AllowAny,))
-def register(request):
-    if request.method == 'POST':
-        Name = request.POST.get('name')
-        Email = request.POST.get('email')
-        Phone = request.POST.get('phone')
-        Homeadd = request.POST.get('homeadd')
-        Carno = request.POST.get('carno')
-        Pw = request.POST.get('pass')
-        user_obj = User()
-        user_obj.name = Name
-        user_obj.email = Email
-        user_obj.phone = Phone
-        user_obj.home_add = Homeadd
-        user_obj.carno = Carno
-        user_obj.password = Pw
-        user_obj.rating = 0.0
-        user_obj.save(force_insert=True)
-        return Response("success")
-
 #curl -X POST -F id=7 -F slat=ew -F slong=q -F dlat=eww -F dlong -F ts=ewww  192.168.1.103:8000/api/request
-
 
 @api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
 def passenger_request(request):
     request_id = request.data['id']
     newRequest = Trip_Request()
-   # req = UserSerializer.objects.get(id = request_id)
     driver(request_id)
     req = User.objects.get(id = request_id)
     newRequest.request_id = request_id
@@ -95,7 +34,6 @@ def passenger_request(request):
     newRequest.phone_no = req.phone
     newRequest.save()
     string = driver(request_id)
-
     return HttpResponse(string)
 
 
@@ -148,29 +86,14 @@ def endjourney(request):  # Function to finish journey, called when the passenge
 
 @api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
-def finish(request): # Function to delete request record called after completion of request when driver id is received by passenger's app
+def complete(request): # Function to delete request record called after completion of request when driver id is received by passenger's app
     Id = request.data['id']
     Trip_Request.objects.get(request_id = Id).delete()
     return HttpResponse("Done")
 
 
-def checkmatch(request):
-        if request.method == 'POST':
-            id = request.POST.get('email', '')
-            pwd = request.POST.get('pass', '')
-            checkuser = UserSerializer.objects.all().filter(email=id)
-            if pwd == checkuser.password:
-                return Response(status=status.HTTP_202_ACCEPTED)
-            else:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-@permission_classes((permissions.AllowAny,))
-def getuser(request):
-        Id = request.data['id']
-        req = UserSerializer.objects.get(pk=Id)
-        return HttpResponse(json.dumps(req.as_json()), content_type="application/json")
 
 
 
--- urls.py
+++ urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import url, include
 from django.conf.urls import patterns
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets, routers
-from passenger_requests import urls
+from passenger_requests import views as pviews
+from testing import views as testingviews
+from example.views import ListCreateTestClass
+from example.estimatedFareCalculationModule import getEstFare
+from example.actualFareCalculationModule import getActFare
+
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -20,4 +25,21 @@ class UserViewSet(viewsets.ModelViewSet):
 # Routers provide a way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-urlpatterns = patterns('',(r'^', include('example.urls')))
+urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
+    url(r'^checkmatch$', testingviews.checkmatch, name='checkmatch'),
+    url(r'^register$', testingviews.register, name='register'),
+    url(r'^checkproof$', testingviews.checkproof, name='checkproof'),
+    url(r'^picupload$', testingviews.picupload, name='picupload'),
+    url(r'^forgetpass$', testingviews.forgetpass, name='forgetpass'),
+    url(r'^api/request', pviews.passenger_request),
+    url(r'^api/status', pviews.get_status),
+    url(r'^api/driver', pviews.get_driver),
+    url(r'^api/response', pviews.driver_response),
+    url(r'^api/endjourney', pviews.endjourney ),
+    url(r'^api/complete', pviews.complete),
+    url(r'^api/get_passenger', pviews.get_passenger),
+    url(r'^api/estimatefare', getEstFare),
+    url(r'^api/actualfare', getActFare)
+]
